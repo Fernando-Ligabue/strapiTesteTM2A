ac97e09aa501dc1d05ab1f6d7560b34c3a2fb0f4
Fernando Ligabue <nandoligabue@gmail.com>
2025-10-06 16:55:29 +0100

    ALTER- ficheiro middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,6 +2,17 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        // 'https://seu-frontend-render.com', // URL do seu frontend
+        'http://localhost:3000', // URL do seu frontend local (em desenvolvimento)
+      ],
+      headers: ['Content-Type', 'Authorization', 'X-Requested-With'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
